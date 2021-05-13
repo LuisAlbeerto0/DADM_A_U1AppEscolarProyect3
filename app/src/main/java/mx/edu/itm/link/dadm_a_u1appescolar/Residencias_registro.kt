bb0a9efba7d707490fca78b97ca8f57fb94cb831
Luis Alberto <luis-demo@hotmail.com>
2021-05-12 22:57:10 -0500

    Se agrego el modulo de proyectos y registrar proyecto

--- app/src/main/java/mx/edu/itm/link/dadm_a_u1appescolar/Residencias_registro.kt
+++ app/src/main/java/mx/edu/itm/link/dadm_a_u1appescolar/Residencias_registro.kt
@@ -1,11 +1,34 @@
 package mx.edu.itm.link.dadm_a_u1appescolar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Residencias_registro : AppCompatActivity() {
+   // lateinit var btnResP : Button
+    //lateinit var btnResR : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_residencias_registro2)
+
+        val btnResP: Button = findViewById(R.id.btnResP)
+        val btnResR: Button = findViewById(R.id.btnResR)
+
+
+
+        btnResP.setOnClickListener {
+            invocarActivity(Proyectos_disponibles::class.java)
+
+        }
+        btnResR.setOnClickListener {
+                invocarActivity(Registrar_proyectos::class.java)
+            }
+
+    }
+    private fun invocarActivity(clase : Class<*>) {
+        val intent = Intent(this,clase)
+        startActivityForResult(intent, 1)
     }
 }
