d9479a27216b34caf3b7da7ba7bc29d2989b2218
Gabriel S. Ruiz C <gabo17121102@gmail.com>
2021-05-22 18:59:10 -0500

    Registro de proyectos V1.0

--- app/src/main/java/mx/edu/itm/link/dadm_a_u1appescolar/Registrar_proyectos.kt
+++ app/src/main/java/mx/edu/itm/link/dadm_a_u1appescolar/Registrar_proyectos.kt
@@ -1,14 +1,55 @@
 package mx.edu.itm.link.dadm_a_u1appescolar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import org.json.JSONObject
 
 class Registrar_proyectos : AppCompatActivity() {
+
+    lateinit var editNombre : EditText
+    lateinit var editDescripcion : EditText
+    lateinit var editEncargado : EditText
+    lateinit var btnGuardarProyecto : Button
+    lateinit var btnSalir : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar_proyectos)
 
+        editNombre = findViewById(R.id.editNombre)
+        editDescripcion = findViewById(R.id.editdescripcion)
+        editEncargado = findViewById(R.id.editEncargado)
+
+        btnGuardarProyecto = findViewById(R.id.btnGuardarProyecto)
+        btnSalir = findViewById(R.id.btnSalir)
+
+        btnSalir.setOnClickListener { finish() }
+
+        val stringBD = intent.getStringExtra("bd")
+
+        var jsonBD = JSONObject(stringBD)
+
+        btnGuardarProyecto.setOnClickListener {
+            val json = JSONObject()
+            json.put("Nombre_proyecto", editNombre.text.toString())
+            json.put("Descripcion", editDescripcion.text.toString())
+            json.put("Encargado", editEncargado.text.toString())
 
 
+            val Proyectos = jsonBD.getJSONArray("proyectos")
+            Proyectos.put(json)
+
+            jsonBD = JSONObject()
+            jsonBD.put("proyectos", Proyectos)
+
+            val intent = Intent(this, MenuActivity::class.java)
+            intent.putExtra("proyectos", json.toString())
+            intent.putExtra("bd", jsonBD.toString())
+            startActivity(intent)
+            finish()
+        }
     }
 }
